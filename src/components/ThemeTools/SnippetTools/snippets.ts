f7305e2b24aeab76f606b23b8e0539d1c4c75b44
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2022-11-01 09:46:16 -0600

    Updates

--- src/components/ThemeTools/SnippetTools/snippets.ts
+++ src/components/ThemeTools/SnippetTools/snippets.ts
@@ -82,39 +82,37 @@ const snippets: Array<SnippetModification> = [
       },
     ],
   },
+  // {
+  //   title: "Gradient Buttons",
+  //   configs: [
+  //     {
+  //       path: "components.MuiButton.styleOverrides",
+  //       value: {
+  //         root: {
+  //           background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+  //           border: 0,
+  //           borderRadius: 3,
+  //           boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  //           color: "white",
+  //           height: 48,
+  //           padding: "0 30px",
+  //         },
+  //       },
+  //     },
+  //   ],
+  // },
   {
-    title: "Button Override",
+    title: "Button Override Classes",
+    info: "Override component classes. CSS information:",
+    docs: "https://mui.com/material-ui/api/button/#css",
     configs: [
       {
-        path: "overrides.MuiButton",
+        
+        path: "components.MuiButton.styleOverrides.root",
         value: {
-          root: {
-            width: 42,
-            height: 26,
-            padding: 0,
-            margin: 8,
-          },
+
         },
-      },
-    ],
-  },
-  {
-    title: "Gradient Buttons",
-    configs: [
-      {
-        path: "components.MuiButton.styleOverrides",
-        value: {
-          root: {
-            background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-            border: 0,
-            borderRadius: 3,
-            boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-            color: "white",
-            height: 48,
-            padding: "0 30px",
-          },
-        },
-      },
+      }
     ],
   },
   {
